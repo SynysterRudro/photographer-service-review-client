44d12338217299389cd0464c91c09398deaba70c
SynysterRudro <rudrosingh82@gmail.com>
2022-11-10 03:21:46 +0600

    service.json file added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div className="hero h-80" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-white">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Photo Mania</h1>
+                    <p className="mb-5">A personal photography service. A platform where customers can see Rudro's photography services and can give reviews unbiasedly and also see others review</p>
+                    <Link to='/services'><button className="btn btn-primary">Services</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Routes/Routes/Router.js
+++ src/Routes/Routes/Router.js
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Services from '../../Pages/Home/Services/Services'
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main></Main>
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/services',
+                element: <Services></Services>
+            }
+        ]
     }
 ])
