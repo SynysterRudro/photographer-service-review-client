2ecda05c80c443cbb1fb5f8e79464bbfeaaf8d48
SynysterRudro <rudrosingh82@gmail.com>
2022-11-10 04:29:16 +0600

    works of service route completed

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from '../Home/Services/ServiceCard';
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className='my-9'>
+            <h2 className="text-5xl text-orange-500 text-center font-bold">Services</h2>
+            <div className='grid my-9 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+
+            </div>
         </div>
     );
 };
